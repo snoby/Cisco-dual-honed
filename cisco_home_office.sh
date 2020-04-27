caf3894092c30510fe670e0ae1cba90bbfbdbc33
iotapi322 <matt.snoby@icloud.com>
2020-04-27 15:04:35 -0400

    added cisco keeper subnet

--- cisco_home_office.sh
+++ cisco_home_office.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# This a script to setup a dual honed MAC to connect to a public internet available network (Home Network)
+# This a script to setup a dual honed MAC to connect to a public internet available network (Sonnet Network)
 # and to connect to an ethernet connected private network.  (VPN network).  This is through a second router
 # that has access to the private network.
 #
@@ -27,6 +27,10 @@ cat <<HEREDOC
 
   That way I can just call 'cisco-on' or 'cisco-off' when I want too.
 
+  -----------------------------------------------------------
+  Pass in cisco-only to make cisco interface top priority
+  Pass in home-only to make the home interface top priority
+
 HEREDOC
 
 }
@@ -36,10 +40,12 @@ if [ -z "$1" ]; then usage; exit; fi
 if [ "--help" == "$1" ]; then usage; exit; fi
 
 CISCO_INTERFACE=en8
+CISCO_DNS=64.102.6.247
+HOME_INTERFACE="Sonnet"
 HOME_DNS=10.0.0.1
 
 create_route() {
-sudo networksetup -setnetworkserviceenabled Home on
+sudo networksetup -setnetworkserviceenabled "$HOME_INTERFACE" on
 sudo sysctl -w net.inet.ip.forwarding=1
 
 
@@ -56,8 +62,11 @@ sudo route -n add -net 171.69 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 171.70 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 171.71 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 161.144 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 114.29 -interface "${CISCO_INTERFACE}"
 #
+sudo route -n add -net 3.13  -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 3.16  -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 3.228  -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.1.0.0/16  -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.2.0.0/15  -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.4.0.0/14  -interface "${CISCO_INTERFACE}"
@@ -75,8 +84,11 @@ sudo route -n add -net 10.224.0.0/32 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.226.0.0/16 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.230 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 10.252 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 13.59.86/22 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 13.59.223/24 -interface "${CISCO_INTERFACE}"
 sudo route -n add -net 13.56.118.0/24 -interface "${CISCO_INTERFACE}"
+
+sudo route -n add -net 18.211.0.0/14  -interface "${CISCO_INTERFACE}"
 
 #
 
@@ -122,8 +134,8 @@ sudo route -n add -net 52.0.0.0/6 -interface "${CISCO_INTERFACE}"
 #
 # Set DNS for Cisco networks
 #
-#sudo networksetup -setdnsservers Home 64.102.6.247 "${HOME_DNS}"
-sudo networksetup -setdnsservers Home 161.44.124.122 "${HOME_DNS}"
+#sudo networksetup -setdnsservers Sonnet 64.102.6.247 "${HOME_DNS}"
+sudo networksetup -setdnsservers "$HOME_INTERFACE" "$CISCO_DNS" "${HOME_DNS}"
 sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache
 
 }
@@ -131,9 +143,12 @@ sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheuti
 delete_route() {
 
 sudo sysctl -w net.inet.ip.forwarding=1
-#sudo networksetup -setnetworkserviceenabled Home off
+#sudo networksetup -setnetworkserviceenabled Sonnet off
 # delete routes for cisco specific servers that we know about.
 
+sudo route -n delete -net 3.13  -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 3.228  -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 148.62.40.0/24 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 149.96 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 173.36 -interface "${CISCO_INTERFACE}"
@@ -146,6 +161,7 @@ sudo route -n delete -net 171.70 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 171.71 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 161.44 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 161.144 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 114.29 -interface "${CISCO_INTERFACE}"
 #
 sudo route -n delete -net 10.1.0.0/16  -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 10.2.0.0/15  -interface "${CISCO_INTERFACE}"
@@ -164,6 +180,7 @@ sudo route -n delete -net 10.83.6.32/27 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 10.128.0.0/10 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 10.192.0.0/11 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 10.224.0.0/32 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 18.211.0.0/14  -interface "${CISCO_INTERFACE}"
 #
 sudo route -n delete -net 23.96.0.0/13 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 52.96.0.0/16 -interface "${CISCO_INTERFACE}"
@@ -189,6 +206,7 @@ sudo route -n delete -net 172.16 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 172.18 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 192.168.111 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 199.91.0.0/16 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 13.59.86/22 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 13.59.223/24 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 13.56.118.0/24 -interface "${CISCO_INTERFACE}"
 sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE}"
@@ -201,7 +219,7 @@ sudo route -n delete -net 217.70.184.38/31 -interface "${CISCO_INTERFACE}"
   #
   # Set DNS for Cisco networks
   #
-  sudo networksetup -setdnsservers Home "${HOME_DNS}" 8.8.8.8
+  sudo networksetup -setdnsservers "$HOME_INTERFACE" "${HOME_DNS}"
   sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache
 }
 
@@ -210,8 +228,25 @@ sudo route -n delete -net 217.70.184.38/31 -interface "${CISCO_INTERFACE}"
 #
 # Enable ip forwarding
 
+cisco_only() {
+  networksetup -ordernetworkservices "$CISCO_INTERFACE" "$HOME_INTERFACE" "Wi-Fi"
+}
+
+home_only() {
+  networksetup -ordernetworkservices "$CISCO_INTERFACE" "$HOME_INTERFACE" "Wi-Fi"
+}
 
   case "$1" in
+  'cisco-only')
+  echo "Going Full Cisco mode:  ${CISCO_INTERFACE}"
+  delete_route
+  cisco_only
+  ;;
+  'home-only')
+  echo "Going Full Home Mode"" ${HOME_INTERFACE}"
+  delete_route
+  home_only
+  ;;
   'start')
   echo "Creating Static Routes on interface "${CISCO_INTERFACE}""
   create_route
