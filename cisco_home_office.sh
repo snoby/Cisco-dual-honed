f96fe1df467abddb5ef2dee42507443e1a48dedb
iotapi322 <matt.snoby@icloud.com>
2020-05-28 10:43:57 -0400

    interface name and device id's are self discovered

--- cisco_home_office.sh
+++ cisco_home_office.sh
@@ -41,17 +41,17 @@ HEREDOC
 if [ -z "$1" ]; then usage; exit; fi
 if [ "--help" == "$1" ]; then usage; exit; fi
 
-CISCO_INTERFACE=en8
+#CISCO_INTERFACE=en8
 CISCO_DNS=64.102.6.247
-HOME_INTERFACE="10G"
+#HOME_INTERFACE="10G"
 HOME_DNS=10.0.0.1
 
 
 HOME_INTERFACE_NAME="10G"
 CISCO_INTERFACE_NAME="Cisco"
 
-HOME_INTERFACE_DEVICE_ID=$(networksetup -listnetworkserviceorder | grep "$HOME_INTERFACE_NAME" -A2 | awk -F ':' '{print substr($3, 1, length($3)-1)}' | tr -d '\n')
-CISCO_INTERFACE_DEVICE_ID=$(networksetup -listnetworkserviceorder | grep "$CISCO_INTERFACE_NAME" -A2 | awk -F ':' '{print substr($3, 1, length($3)-1)}' | tr -d '\n')
+HOME_INTERFACE_DEVICE_ID=$(networksetup -listnetworkserviceorder | grep "$HOME_INTERFACE_NAME" -A2 | awk -F ':' '{print substr($3, 1, length($3)-1)}' | tr -d '\n' | tr -d "[:space:]")
+CISCO_INTERFACE_DEVICE_ID=$(networksetup -listnetworkserviceorder | grep "$CISCO_INTERFACE_NAME" -A2 | awk -F ':' '{print substr($3, 1, length($3)-1)}' | tr -d '\n'| tr -d "[:space:]")
 
 
 echo "HOME INTERFACE_DEVICE_ID = $HOME_INTERFACE_DEVICE_ID"
@@ -61,7 +61,7 @@ echo "CISCO INTERFACE_DEVICE_ID = $CISCO_INTERFACE_DEVICE_ID"
 NEW=()
 
 create_route() {
-sudo networksetup -setnetworkserviceenabled "$HOME_INTERFACE" on
+sudo networksetup -setnetworkserviceenabled "$HOME_INTERFACE_NAME" on
 sudo sysctl -w net.inet.ip.forwarding=1
 
 
@@ -69,89 +69,89 @@ sudo sysctl -w net.inet.ip.forwarding=1
 
 
 # Add routes for cisco specific servers that we know about.
-sudo route -n add -net 173.36 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 173.37 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 173.38 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 173.39 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 171.68 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 171.69 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 171.70 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 171.71 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 161.144 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 114.29 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 173.36 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 173.37 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 173.38 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 173.39 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 171.68 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 171.69 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 171.70 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 171.71 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 161.144 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 114.29 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 #
-sudo route -n add -net 3.13  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 3.16  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 3.228  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.1.0.0/16  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.2.0.0/15  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.4.0.0/14  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.8.0.0/13   -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.16.0.0/12  -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.32.0.0/13  -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 3.13  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 3.16  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 3.228  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.1.0.0/16  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.2.0.0/15  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.4.0.0/14  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.8.0.0/13   -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.16.0.0/12  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.32.0.0/13  -interface "${CISCO_INTERFACE_DEVICE_ID}"
 #
-sudo route -n add -net 10.40.0.0/13 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.48.0.0/12 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.83.6.32/27 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.64.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.128.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.192.0.0/11 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.224.0.0/32 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.226.0.0/16 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.230 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 10.252 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 13.59.86/22 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 13.59.223/24 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 13.56.118.0/24 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 10.40.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.48.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.83.6.32/27 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.64.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.128.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.192.0.0/11 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.224.0.0/32 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.226.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.230 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 10.252 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 13.59.86/22 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 13.59.223/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 13.56.118.0/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 18.211.0.0/14  -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 18.211.0.0/14  -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
 #
 
-sudo route -n add -net 23.96.0.0/13 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 52.96.0.0/16 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 23.96.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 52.96.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 40.64.0.0/10 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 40.64.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 64.100.0.0/14 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 64.104.0.0/16 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 64.112.0.0/12 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 64.128.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 64.192.0.0/11 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 64.224.0.0/32 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 64.100.0.0/14 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 64.104.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 64.112.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 64.128.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 64.192.0.0/11 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 64.224.0.0/32 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 66.114.0.0/12 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 69.162.64.0/18 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 66.114.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 69.162.64.0/18 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
 
-sudo route -n add -net 72.163 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 72.163 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 104.208.0.0/13 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 104.208.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n add -net 148.62.40.0/24 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 149.96 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 161.44 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 172.16 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 172.18 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 192.168.111 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 199.91.0.0/16 -interface "${CISCO_INTERFACE}"
-sudo route -n add -net 207.182.170/16 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 148.62.40.0/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 149.96 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 161.44 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 172.16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 172.18 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 192.168.111 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 199.91.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n add -net 207.182.170/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
 #
 # because Cisco throttles uploads to dockerhub
 # we route dockerhub through the VPN interface
 #
-sudo route -n add -net 217.70.184.38/31 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 217.70.184.38/31 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
 # for aws cloud instances
 
-sudo route -n add -net 52.0.0.0/6 -interface "${CISCO_INTERFACE}"
+sudo route -n add -net 52.0.0.0/6 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 #
 #
 # Set DNS for Cisco networks
 #
 #sudo networksetup -setdnsservers Sonnet 64.102.6.247 "${HOME_DNS}"
-sudo networksetup -setdnsservers "$HOME_INTERFACE" "$CISCO_DNS" "${HOME_DNS}"
+sudo networksetup -setdnsservers "$HOME_INTERFACE_NAME" "$CISCO_DNS" "${HOME_DNS}"
 sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache
 
 }
@@ -162,80 +162,80 @@ sudo sysctl -w net.inet.ip.forwarding=1
 #sudo networksetup -setnetworkserviceenabled Sonnet off
 # delete routes for cisco specific servers that we know about.
 
-sudo route -n delete -net 3.13  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 3.228  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 148.62.40.0/24 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 149.96 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 173.36 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 173.37 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 173.38 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 173.39 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 171.68 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 171.69 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 171.70 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 171.71 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 161.44 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 161.144 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 114.29 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 3.13  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 3.228  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 148.62.40.0/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 149.96 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 173.36 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 173.37 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 173.38 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 173.39 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 171.68 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 171.69 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 171.70 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 171.71 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 161.44 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 161.144 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 114.29 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 #
-sudo route -n delete -net 10.1.0.0/16  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.2.0.0/15  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.4.0.0/14  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.8.0.0/13   -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.16.0.0/12  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.32.0.0/13  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.252 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.230 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.226.0.0/16 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 10.1.0.0/16  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.2.0.0/15  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.4.0.0/14  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.8.0.0/13   -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.16.0.0/12  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.32.0.0/13  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.252 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.230 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.226.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 10.40.0.0/13 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.48.0.0/12 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.64.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.83.6.32/27 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.128.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.192.0.0/11 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 10.224.0.0/32 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 18.211.0.0/14  -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 10.40.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.48.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.64.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.83.6.32/27 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.128.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.192.0.0/11 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 10.224.0.0/32 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 18.211.0.0/14  -interface "${CISCO_INTERFACE_DEVICE_ID}"
 #
-sudo route -n delete -net 23.96.0.0/13 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 52.96.0.0/16 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 23.96.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 52.96.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
 
-sudo route -n delete -net 40.64.0.0/10 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 40.64.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 64.100.0.0/14 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 64.104.0.0/16 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 64.112.0.0/12 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 64.128.0.0/10 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 64.192.0.0/11 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 64.224.0.0/32 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 66.114.0.0/12 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 64.100.0.0/14 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 64.104.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 64.112.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 64.128.0.0/10 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 64.192.0.0/11 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 64.224.0.0/32 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 66.114.0.0/12 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 69.162.64.0/18 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 69.162.64.0/18 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 72.163 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 72.163 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 104.208.0.0/13 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 104.208.0.0/13 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 172.16 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 172.18 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 192.168.111 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 199.91.0.0/16 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 13.59.86/22 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 13.59.223/24 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 13.56.118.0/24 -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE}"
-sudo route -n delete -net 207.182.170/16 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 172.16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 172.18 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 192.168.111 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 199.91.0.0/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 13.59.86/22 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 13.59.223/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 13.56.118.0/24 -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 3.16  -interface "${CISCO_INTERFACE_DEVICE_ID}"
+sudo route -n delete -net 207.182.170/16 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 
-sudo route -n delete -net 217.70.184.38/31 -interface "${CISCO_INTERFACE}"
+sudo route -n delete -net 217.70.184.38/31 -interface "${CISCO_INTERFACE_DEVICE_ID}"
 # for aws cloud instances
 
-  sudo route -n delete -net 52.0.0.0/6 -interface "${CISCO_INTERFACE}"
+  sudo route -n delete -net 52.0.0.0/6 -interface "${CISCO_INTERFACE_DEVICE_ID}"
   #
   # Set DNS for Cisco networks
   #
-  sudo networksetup -setdnsservers "$HOME_INTERFACE" "${HOME_DNS}"
+  sudo networksetup -setdnsservers "$HOME_INTERFACE_NAME" "${HOME_DNS}"
   sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache
 }
 
@@ -283,7 +283,7 @@ home_only() {
 
   case "$1" in
   'cisco-only')
-  echo "Going Full Cisco mode:  ${CISCO_INTERFACE}"
+  echo "Going Full Cisco mode:  ${CISCO_INTERFACE_DEVICE_ID}"
   delete_route
   cisco_only
   ;;
@@ -293,7 +293,7 @@ home_only() {
   home_only
   ;;
   'start')
-  echo "Creating Static Routes on interface "${CISCO_INTERFACE}""
+  echo "Creating Static Routes on interface "${CISCO_INTERFACE_DEVICE_ID}""
   create_route
   ;;
   'stop')
